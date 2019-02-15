5d13d88d01cc788db5b01c6e202bfc156396c48e
Jose Figueredo <josefigueredo@gmail.com>
2019-02-15 15:29:41 -0300

    Update main.go

--- main.go
+++ main.go
@@ -6,48 +6,18 @@ import (
 	"net/http"
 	"net/http/httputil"
 	"net/url"
+	"strconv"
 	"strings"
 )
 
-
-/* This allows to modifiy the proxied response before sending it to the requester
-type transport struct {
-	http.RoundTripper
-}
-
-func (t *transport) RoundTrip(req *http.Request) (resp *http.Response, err error) {
-	resp, err = t.RoundTripper.RoundTrip(req)
-	if err != nil {
-		return nil, err
-	}
-	b, err := ioutil.ReadAll(resp.Body)
-	if err != nil {
-		return nil, err
-	}
-	err = resp.Body.Close()
-	if err != nil {
-		return nil, err
-	}
-	b = bytes.Replace(b, []byte("some text or patter to replace"), []byte("the replacement"), -1)
-	body := ioutil.NopCloser(bytes.NewReader(b))
-	resp.Body = body
-	resp.ContentLength = int64(len(b))
-	resp.Header.Set("Content-Length", strconv.Itoa(len(b)))
-	return resp, nil
-}
-*/
-
 func serveReverseProxy(newHost string, path string, w http.ResponseWriter, req *http.Request) {
-
 	url, _ := url.Parse(newHost)
-
 	proxy := httputil.NewSingleHostReverseProxy(url)
-	// proxy.Transport = &transport{http.DefaultTransport}
 
-	req.URL.Host   = url.Host
+	req.URL.Host = url.Host
 	req.URL.Scheme = url.Scheme
-	req.URL.Path   = path
-	req.Host       = url.Host
+	req.URL.Path = path
+	req.Host = url.Host
 	req.RequestURI = ""
 	req.Header.Set("X-Forwarded-Host", req.Header.Get("Host"))
 
@@ -57,22 +27,25 @@ func serveReverseProxy(newHost string, path string, w http.ResponseWriter, req *
 func handleRequestAndRedirect(w http.ResponseWriter, r *http.Request) {
 	fmt.Printf("Req: %s %s\n", r.Host, r.URL.Path)
 
-	url     := fmt.Sprintf("http://%s", r.Host)
-	newHost := strings.Replace(url, listeningPort, forwardingPort, 1)
-	path    := fmt.Sprintf("%s", r.URL.Path)
+	url := fmt.Sprintf("http://%s", r.Host)
+	newHost := strings.Replace(url, strconv.Itoa(listeningPort), strconv.Itoa(forwardingPort), 1)
+	path := fmt.Sprintf("%s", r.URL.Path)
 
 	serveReverseProxy(newHost, path, w, r)
 }
 
-var listeningPort = ":8032"
-var forwardingPort = ":8088"
+var listeningPort int
+var forwardingPort int
 
 func main() {
-	listeningPort = *flag.String("listeningPort", ":8032", "The port to open")
-	forwardingPort   = *flag.String("forwardingPort", ":8088", "The port to forward")
+	flag.IntVar(&listeningPort, "listeningPort", 8032, "The port to open")
+	flag.IntVar(&forwardingPort, "forwardingPort", 8088, "The port to forward")
+	flag.Parse()
+	fmt.Println("listeningPort:  ", listeningPort)
+	fmt.Println("forwardingPort: ", forwardingPort)
 
 	http.HandleFunc("/", handleRequestAndRedirect)
-	if err := http.ListenAndServe(listeningPort, nil); err != nil {
+	if err := http.ListenAndServe(":"+strconv.Itoa(listeningPort), nil); err != nil {
 		panic(err)
 	}
 }
